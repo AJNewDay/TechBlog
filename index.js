52c33ad84bc19f09c460c1de5d8d4dae84dfd25c
Aaron Jordan <aabjordan88@gmail.com>
2023-04-17 14:30:01 -0400

    2:29pm

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-//Loads the express module
-const express = require('express');
+//Loads the handlebars module
 const handlebars = require('express-handlebars');
-//Creates our express server
+const express = require('express');
 const app = express();
 const port = 3000;
-//Serves static files (we need it to import a css file)
-app.use(express.static('public'));
-//Sets a basic route
-app.get('/', (req, res) => res.send('TechBlog Running'));
 
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
 app.engine(
-  'hbs',
-  handlebars({
+  'handlebars',
+  handlebars.engine({
     layoutsDir: __dirname + '/views/layouts',
-
-    //new configuration parameter
-    partialsDir: __dirname + '/views/partials/',
   })
 );
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  //Using the index.hbs file instead of planB
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
   res.render('main', { layout: 'index' });
 });
 
-//Makes the app listen to port 3000
 app.listen(port, () => console.log(`App listening to port ${port}`));
